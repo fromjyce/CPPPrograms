d4017e7a63c2de41d2b88887d9b28d3569a8cd2e
Jayashre <jaya2004kra@gmail.com>
2024-09-13 23:03:51 +0530

    added serial code

--- High Performance Computing/CIA-I/backend/cpp_implementations/dij_serial.cpp
+++ High Performance Computing/CIA-I/backend/cpp_implementations/dij_serial.cpp
@@ -1,69 +1,77 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <climits>
+#include <limits>
 #include <chrono>
+#include <map>
+#include <string>
+#include <algorithm>
 
 using namespace std;
+using namespace std::chrono;
 
-typedef pair<int, int> pii;
-vector<int> dijkstra(int num_vertices, const vector<vector<pii>>& graph, int source) {
-    vector<int> dist(num_vertices, INT_MAX);
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-
-    dist[source] = 0;
-    pq.push({0, source});
-
-    while (!pq.empty()) {
-        int current_dist = pq.top().first;
-        int current_vertex = pq.top().second;
-        pq.pop();
-
-        if (current_dist > dist[current_vertex]) {
-            continue;
+template <typename T>
+T minDistance(const map<T, int>& dist, const vector<T>& nodes) {
+    T minNode;
+    int minDist = numeric_limits<int>::max();
+    
+    for (const T& node : nodes) {
+        if (dist.at(node) < minDist) {
+            minDist = dist.at(node);
+            minNode = node;
         }
+    }
+    return minNode;
+}
 
-        for (const auto& edge : graph[current_vertex]) {
-            int neighbor = edge.first;
-            int weight = edge.second;
+template <typename T>
+void dijkstra(const map<T, vector<pair<T, int>>>& graph, const T& startNode) {
+    map<T, int> dist;
+    map<T, T> prev;
+    vector<T> nodes;
+    
+    for (const auto& pair : graph) {
+        T node = pair.first;
+        nodes.push_back(node);
+        dist[node] = numeric_limits<int>::max();
+        prev[node] = T();
+    }
+    
+    dist[startNode] = 0;
+    while (!nodes.empty()) {
+        T u = minDistance<T>(dist, nodes);
+        nodes.erase(remove(nodes.begin(), nodes.end(), u), nodes.end());
 
-            if (dist[current_vertex] + weight < dist[neighbor]) {
-                dist[neighbor] = dist[current_vertex] + weight;
-                pq.push({dist[neighbor], neighbor});
+        for (const auto& neighbor : graph.at(u)) {
+            T v = neighbor.first;
+            int weight = neighbor.second;
+            if (dist[u] != numeric_limits<int>::max() && dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+                prev[v] = u;
             }
         }
     }
 
-    return dist;
+    for (const auto& pair : dist) {
+        cout << "Distance from " << startNode << " to " << pair.first << " is " << pair.second << endl;
+    }
 }
 
 int main() {
-    int num_vertices, num_edges;
-    cout << "Enter the number of vertices: ";
-    cin >> num_vertices;
-    cout << "Enter the number of edges: ";
-    cin >> num_edges;
-    vector<vector<pii>> graph(num_vertices);
-
-    cout << "Enter the edges in the format (u v w) where u and v are vertices (0-indexed) and w is the weight:\n";
-    for (int i = 0; i < num_edges; ++i) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        graph[u].push_back({v, w});
-        graph[v].push_back({u, w});
-    }
-
-    int source;
-    cout << "Enter the source vertex: ";
-    cin >> source;
-    auto start = chrono::high_resolution_clock::now();
-    vector<int> distances = dijkstra(num_vertices, graph, source);
-    auto end = chrono::high_resolution_clock::now();
-    chrono::duration<double> duration = end - start;
-    cout << "Time taken by serial Dijkstra's algorithm: " << duration.count() << " seconds" << endl;
-    for (int i = 0; i < num_vertices; ++i) {
-        cout << "Distance from vertex " << source << " to vertex " << i << " is " << distances[i] << endl;
-    }
+    map<string, vector<pair<string, int>>> graph = {
+        {"A", {{"B", 4}, {"C", 5}}},
+        {"B", {{"A", 4}, {"C", 11}, {"D", 7}}},
+        {"C", {{"A", 5}, {"B", 11}, {"D", 3}}},
+        {"D", {{"B", 7}, {"C", 3}}}
+    };
+    
+    string startNode = "A";
+    
+    auto start = high_resolution_clock::now();
+    dijkstra(graph, startNode);
+    auto end = high_resolution_clock::now();
+    
+    auto duration = duration_cast<microseconds>(end - start).count();
+    cout << "Serial version took " << duration << " microseconds." << endl;
 
     return 0;
 }
