fedf3cb82029d8bf1ef350801453bf9d96fb29cd
Jayashre <jaya2004kra@gmail.com>
2024-09-13 23:20:01 +0530

    added route for result

--- High Performance Computing/CIA-I/frontend/src/App.js
+++ High Performance Computing/CIA-I/frontend/src/App.js
@@ -1,11 +1,13 @@
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Results from "./components/Results";
 
 function App() {
   return (
     <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/results" element={<Results />} />
             </Routes>
         </Router>
   );
